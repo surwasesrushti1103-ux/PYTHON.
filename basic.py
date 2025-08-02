72b37acbae69079ed305c43c11178c0961879d77
Shrushti Surwase <surwasesrushti1103@gmail.com>
2025-08-02 21:00:38 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,21 @@
+#Firstcode
+print("HELLO EVERYONE")
+name="SHRUSHTI"
+print(f"HELLO,{name}")
+
+#addition
+a = 5
+b = 3
+sum = a + b
+print("Sum is:", sum)
+
+#Checkmemorykaddress
+x=10
+y=10
+print(x,y)
+print(id(x),id(y))
+
+#concatenation
+a= "Hello"
+b= "Shrushti"
+print(a+" "+b)
