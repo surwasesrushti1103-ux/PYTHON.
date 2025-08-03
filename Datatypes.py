e81022f3f6e1c73bed731ee07e1c1d2f8a0f934d
Shrushti Surwase <surwasesrushti1103@gmail.com>
2025-08-03 14:27:45 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,46 @@
+
+#int
+a=5
+print(a,type(a))
+#float
+a=5.5
+print(a,type(a))
+#complex
+a=2+4j
+print(a,type(a))
+
+#string
+s='Hello 123' 
+print(s,type(s))
+
+s="Good Work"
+print(s,type(s))
+
+s='''
+Welcome 
+To All'''
+
+print(s,type(s))
+
+#List
+l=[1,4.5,'ss']
+l[1]=2
+print(l,type(l))
+
+#Tuple
+t=(5,0,'hello')
+print(t,type(t))
+
+#Dictionary
+
+d= { 
+    'course_name':'python',
+    'course_duration': '2 weeks'
+   }
+print(d['course_name'])
+print(d,type(d))
+
+
+#Set
+A={10,20,3,40}
+print(A ,type(A))
